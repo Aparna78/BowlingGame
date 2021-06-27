16bf078e902a1a68187a824e3333578c77ac7dea
Aparna Kumari <aparnakumari@Aparnas-MacBook-Air.local>
2021-06-27 12:13:33 +0530

    BowlingGame: added lombok lib to auto generate getter/setter,Added score entity class and one to one mapping in player to score entity model

--- src/main/java/com/game/model/Score.java
+++ src/main/java/com/game/model/Score.java
@@ -3,18 +3,16 @@ package com.game.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 
 import lombok.Data;
 
 @Entity
 @Data
-public class Player {
-
-   @Id @GeneratedValue Long id;
-   String name;
-   String laneNumber;
-   @OneToOne
-   Score score;
-  
+public class Score {
+	@Id @GeneratedValue Long id;
+	 Integer totalScore;
+	 Integer currentScore;
+	 Integer numberOfStrike;
+	 Integer numberOfSpare;
+	
 }
