65a700b4aa4e0840d829c91b85f7ac327385a32a
Aparna Kumari <aparnakumari@Aparnas-MacBook-Air.local>
2021-06-27 14:55:46 +0530

    removed unused imports

--- src/test/java/com/game/bowling/BowlingApplicationTests.java
+++ src/test/java/com/game/bowling/BowlingApplicationTests.java
@@ -1,23 +1,17 @@
 package com.game.bowling;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.List;
 
 import javax.transaction.Transactional;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.game.model.Player;
-import com.game.model.Score;
 import com.game.repository.PlayerRepository;
-import com.game.repository.ScoreRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,9 +25,6 @@ class BowlingApplicationTests {
 
 	@Autowired
 	private PlayerRepository playerRepository;
-	
-	@Autowired
-	private ScoreRepository scoreRepository;
 
 	@Test
 	void contextLoads() {
