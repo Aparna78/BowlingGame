c0be20ed7d319d5a9c3696ca3d1329578bb3e891
Aparna Kumari <aparnakumari@Aparnas-MacBook-Air.local>
2021-06-27 17:43:51 +0530

    added rest api to start the game, it will take list of player name as input and return the game id

--- src/main/java/com/game/bowling/BowlingGameController.java
+++ src/main/java/com/game/bowling/BowlingGameController.java
@@ -1,5 +1,6 @@
 package com.game.bowling;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +8,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.game.model.Game;
 import com.game.model.Player;
 import com.game.model.Score;
+import com.game.repository.GameRepository;
 import com.game.repository.PlayerRepository;
 import com.game.repository.ScoreRepository;
+
 
 
 @RestController
@@ -18,10 +22,12 @@ public class BowlingGameController {
 	
 	private final PlayerRepository playerRepository;
 	private final ScoreRepository scoreRepository;
+	private final GameRepository gameRepository;
 
-	BowlingGameController(PlayerRepository playerRepository, ScoreRepository scoreRepository) {
+	BowlingGameController(PlayerRepository playerRepository, ScoreRepository scoreRepository, GameRepository gameRepository) {
 	    this.playerRepository = playerRepository;
 	    this.scoreRepository = scoreRepository;
+	    this.gameRepository = gameRepository;
 	  }
 	
 	//eg: curl -v localhost:8080/players
@@ -34,6 +40,23 @@ public class BowlingGameController {
 	  @GetMapping("/scores")
 	  List<Score> allScore() {
 	    return scoreRepository.findAll();
+	  }
+	  
+	//eg: curl -X POST localhost:8080/startGame -H 'Content-type:application/json' -d '["a","b","c"]'
+	  @PostMapping("/startGame")
+	  String startGame(@RequestBody List<String> playerNameList) {
+	    int noOfPlayer = playerNameList.size();
+	    Game g = new Game();
+	    List<Player> playerList = new ArrayList<Player>();
+	    for (int i=0; i<noOfPlayer; i++) {
+	    	Player p = new Player();
+	    	p.setName(playerNameList.get(i));
+	    	p.setLaneNumber(i%3);
+	    	playerList.add(p);
+	    }
+	    g.setPlayerList(playerList);
+	    gameRepository.save(g);
+	    return "Game with Id" + g.getId() + "is On !!";
 	  }
 	  
 }
--- src/main/java/com/game/model/Game.java
+++ src/main/java/com/game/model/Game.java
@@ -2,17 +2,21 @@ package com.game.model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class Game {
 	
 	@Id @GeneratedValue 
 	Long id;
-	@OneToMany
+	@OneToMany(cascade = {CascadeType.ALL})
 	List<Player> playerList;
 
 }
