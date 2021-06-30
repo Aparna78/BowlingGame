1774821c68b30d52b62e5625452da3ec73c16d6e
Aparna Kumari <aparnakumari@Aparnas-MacBook-Air.local>
2021-06-30 11:51:22 +0530

    modified the client

--- playBowling.py
+++ playBowling.py
@@ -11,17 +11,26 @@ def getTotalScoreByPlayerName(playerName):
     response = requests.get('http://localhost:8080/getTotalScoreByPlayerName/'+playerName)
     return response.text
 
+def playCustom():
+    playBowling(gameId, laneNumber, no_of_pin_knocked)
+    print "Game Update!!"
+
+def sampleGame():
+    playBowling(gameId, laneNumber, 10) #Nairobi's turn and she got strike in her first chance
+    playBowling(gameId, laneNumber, 5)  #Denver's turn
+    playBowling(gameId, laneNumber, 5)  #Denver's second chance and he got spare
+    playBowling(gameId, laneNumber, 8)  #Tokyo's turn
+    playBowling(gameId, laneNumber, 2)  #Tokyo's second chance and she got spare
+    playBowling(gameId, laneNumber, 6)  #Nairobi's turn
+    playBowling(gameId, laneNumber, 2)  #Nairobi's second turn
+    playBowling(gameId, laneNumber, 10) #Denver got strike in his first chance but he will get one extra chance because it is last set
+    playBowling(gameId, laneNumber, 10) #Denver get strike again 
+    playBowling(gameId, laneNumber, 3)  #Tokyo's first chance
+    playBowling(gameId, laneNumber, 4)  #Tokyo's second chanc
+    
+
 gameId = sys.argv[1]
 laneNumber = sys.argv[2]
-playBowling(gameId, laneNumber, 10) #Nairobi's turn and she got strike in her first chance
-print (getTotalScoreByPlayerName('Nairobi'))
-playBowling(gameId, laneNumber, 5)  #Denver's turn
-playBowling(gameId, laneNumber, 5)  #Denver's second chance and he got spare
-playBowling(gameId, laneNumber, 8)  #Tokyo's turn
-playBowling(gameId, laneNumber, 2)  #Tokyo's second chance and she got spare
-playBowling(gameId, laneNumber, 6)  #Nairobi's turn
-playBowling(gameId, laneNumber, 2)  #Nairobi's second turn
-playBowling(gameId, laneNumber, 10) #Denver got strike in his first chance but he will get one extra chance because it is last set
-playBowling(gameId, laneNumber, 10) #Denver get strike again 
-playBowling(gameId, laneNumber, 3)  #Tokyo's first chance
-playBowling(gameId, laneNumber, 4)  #Tokyo's second chance
+no_of_pin_knocked = sys.argv[3]
+#playCustom()
+sampleGame()
--- startGame.py
+++ startGame.py
@@ -1,9 +1,18 @@
 import json
 import requests
 import os
+import sys
+
+def startGameCustom():
+    data = ['Nairobi','Denver','Tokyo']
+    response = requests.post('http://localhost:8080/startGame', json=data)
+    return response.text
 
 def startGame():
-    data = ['Nairobi','Denver','Tokyo']
+    n = int(sys.argv[1])
+    data = []
+    for i in range(1, n+1):
+        data.append(sys.argv[i+1])
     response = requests.post('http://localhost:8080/startGame', json=data)
     return response.text
 
